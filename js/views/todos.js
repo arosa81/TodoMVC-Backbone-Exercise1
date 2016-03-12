3148badf01b813ac7c5e9ae1aa0b9374cae67297
arosa81 <alex.j.rosa@gmail.com>
2016-03-12 16:44:01 -0700

    feat: adding events hash, listenTo model, rendering Todo to template, editing, and saving

--- js/views/todos.js
+++ js/views/todos.js
@@ -9,33 +9,45 @@ app.TodoView = Backbone.View.extend({
 
   // DOM events specific to an item
   events: {
-
+    'dbclick label': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit': 'close'
   },
 
   // The TodoView listens for changes to its model, re-rendering. Since there's
   // a one-to-one correspondence between a **Todo** and a **TodoView** in this
   // app, we set a direct reference on the model for convenience.
   initialize: function() {
-
+    this.listenTo(this.model, 'change', this.render);
   },
 
   // Re-renders the title of the todo item
   render: function() {
-
+    console.log(this.model.attributes);
+    this.$el.html(this.template(this.model.attributes));
   },
 
   // switch view into "editing" mode, displaying input field
   edit: function() {
-
+    this.$el.addClass('editing');
+    this.$input.focus();
   },
 
   // close editing mode, save changes to the todo
   close: function() {
+    var value = this.$input.val().trim();
 
+    if (value) {
+      this.model.save({ title: value });
+    }
+
+    this.$el.removeClass('editing');
   },
 
   // complete editing when hitting enter
-  updateOnEnter: function() {
-
+  updateOnEnter: function(e) {
+    if (e.which === ENTER_KEY) {
+      this.close();
+    }
   }
 });
